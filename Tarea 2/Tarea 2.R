6181110ca3c06987edaa8f8f50950b36e5d6b7d6
Javomgo <javomgo@gmail.com>
2019-05-16 21:58:21 +0200

    16-05

--- Tarea 2/Tarea 2.R
+++ Tarea 2/Tarea 2.R
@@ -7,53 +7,73 @@ hor_rel = matrix(nrow = (length(unique(stock$date)) * length(unique(stock$time))
 q = matrix(nrow = (length(unique(stock$date)) * length(unique(stock$time))), ncol = 1)
 a = matrix(nrow = (length(unique(stock$date)) * length(unique(stock$time))), ncol = 3)
 b = matrix(nrow = (length(unique(stock$date)) * length(unique(stock$time))), ncol = 3)
-
+n = 1
 #Cálculo horquilla relativa
 for (i in 1:length(unique(stock$date))){
   for (j in 1:length(unique(stock$time))){
     #Variable creada para facilitar el almacenamiento en la matriz
     m=1
     #Cálculo del punto medio.
-    q[j*i] = (stock[which(stock$time == stock$time[j] & stock$date == stock$date[i] & stock$sign == 1),]$dvol + 
-                stock[which(stock$time == stock$time[j] & stock$date == stock$date[i] & stock$sign == -1),]$dvol)/2
+    for(l in 1:10){
+    if(stock[which(stock$time == unique(stock$time)[j] & stock$date == unique(stock$date)[i] & stock$sign > 0),][l,]$dvol != 0){
+      for(p in 1:10){
+      if(stock[which(stock$time == unique(stock$time)[j] & stock$date == unique(stock$date)[i] & stock$sign == -p),]$dvol != 0){
+        
+        q[n] = (stock[which(stock$time == unique(stock$time)[j] & stock$date == unique(stock$date)[i]),][l,]$quote + 
+                  stock[which(stock$time == unique(stock$time)[j] & stock$date == unique(stock$date)[i] & stock$sign == -p),]$quote)/2
+        break
+      }
+      }
+      break
+    }   
+  }
     
     for (k in c(100,500,1000)){
       for(l in 1:10){
         #Cálculo del bid ponderado
-        if(sum(stock[which(stock$time == stock$time[j] & stock$date == stock$date[i] & stock$sign == c(1:l)),]$dvol,  
-            stock[which(stock$time == stock$time[j] & stock$date == stock$date[i] & stock$sign == c(1:l)),]$hvol)  >= k &
-           !(sum(stock[which(stock$time == stock$time[j] & stock$date == stock$date[i] & stock$sign == c(1:(l-1))),]$dvol,  
-            stock[which(stock$time == stock$time[j] & stock$date == stock$date[i] & stock$sign == c(1:(l-1))),]$hvol)  >= k)){
+        if(sum(stock[which(stock$time == unique(stock$time)[j] & stock$date == unique(stock$date)[i]),][1:l,]$dvol,  
+            stock[which(stock$time == unique(stock$time)[j] & stock$date == unique(stock$date)[i]),][1:l,]$hvol)  >= k){
           
-          b[(j*i), m] = sum(stock[which(stock$time == stock$time[j] & stock$date == stock$date[i] & stock$sign == c(1:(l-1))),]$quote *
-                  (stock[which(stock$time == stock$time[j] & stock$date == stock$date[i] & stock$sign == c(1:(l-1))),]$dvol +
-                   stock[which(stock$time == stock$time[j] & stock$date == stock$date[i] & stock$sign == c(1:(l-1))),]$hvol), 
-                  stock[which(stock$time == stock$time[j] & stock$date == stock$date[i] & stock$sign == c(l)),]$quote *
-                  (k - sum(stock[which(stock$time == stock$time[j] & stock$date == stock$date[i] & stock$sign == c(1:(l-1))),]$dvol,
-                           stock[which(stock$time == stock$time[j] & stock$date == stock$date[i] & stock$sign == c(1:(l-1))),]$hvol)))/k
+          b[n, m] = sum(stock[which(stock$time == unique(stock$time)[j] & stock$date == unique(stock$date)[i]),][1:l-1,]$quote *
+                  (stock[which(stock$time == unique(stock$time)[j] & stock$date == unique(stock$date)[i]),][1:l-1,]$dvol +
+                   stock[which(stock$time == unique(stock$time)[j] & stock$date == unique(stock$date)[i]),][1:l-1,]$hvol), 
+                  stock[which(stock$time == unique(stock$time)[j] & stock$date == unique(stock$date)[i]),][l,]$quote *
+                  (k - sum(stock[which(stock$time == unique(stock$time)[j] & stock$date == unique(stock$date)[i]),][1:l-1,]$dvol,
+                           stock[which(stock$time == unique(stock$time)[j] & stock$date == unique(stock$date)[i]),][1:l-1,]$hvol)))/k
+          break
+        }else if(stock[which(stock$time == unique(stock$time)[j] & stock$date == unique(stock$date)[i]),][l,3] == -1){
+          #stock[which(stock$time == unique(stock$time)[j] & stock$date == unique(stock$date)[i]),][,3] = NA
         }
       }
-      for(l in -1:-10){
+      if(is.na(stock[which(stock$time == unique(stock$time)[j] & stock$date == unique(stock$date)[i]),][l,3])){break}
+      for(l in 1:10){
         #Cálculo ask ponderado
-        if(sum(stock[which(stock$time == stock$time[j] & stock$date == stock$date[i] & stock$sign == c(-1:l)),]$dvol,  
-               stock[which(stock$time == stock$time[j] & stock$date == stock$date[i] & stock$sign == c(-1:l)),]$hvol)  >= k &
-           !(sum(stock[which(stock$time == stock$time[j] & stock$date == stock$date[i] & stock$sign == c(-1:(l+1))),]$dvol,  
-                 stock[which(stock$time == stock$time[j] & stock$date == stock$date[i] & stock$sign == c(-1:(l+1))),]$hvol)  >= k)){
+        if(sum(stock[which(stock$time == unique(stock$time)[j] & stock$date == unique(stock$date)[i] & stock$sign < 0),][1:l,]$dvol,  
+               stock[which(stock$time == unique(stock$time)[j] & stock$date == unique(stock$date)[i] & stock$sign < 0),][1:l,]$hvol)  >= k){
           
-          a[(j*i), m] = sum(stock[which(stock$time == stock$time[j] & stock$date == stock$date[i] & stock$sign == c(-1:(l+1))),]$quote *
-                             (stock[which(stock$time == stock$time[j] & stock$date == stock$date[i] & stock$sign == c(-1:(l+1))),]$dvol +
-                                stock[which(stock$time == stock$time[j] & stock$date == stock$date[i] & stock$sign == c(-1:(l+1))),]$hvol), 
-                           stock[which(stock$time == stock$time[j] & stock$date == stock$date[i] & stock$sign == c(l)),]$quote *
-                             (k - sum(stock[which(stock$time == stock$time[j] & stock$date == stock$date[i] & stock$sign == c(-1:(l+1))),]$dvol,
-                                      stock[which(stock$time == stock$time[j] & stock$date == stock$date[i] & stock$sign == c(-1:(l+1))),]$hvol)))/k
+          a[n, m] = sum(stock[which(stock$time == unique(stock$time)[j] & stock$date == unique(stock$date)[i] & stock$sign < 0),][1:l-1,]$quote *
+                             (stock[which(stock$time == unique(stock$time)[j] & stock$date == unique(stock$date)[i] & stock$sign < 0),][1:l-1,]$dvol +
+                                stock[which(stock$time == unique(stock$time)[j] & stock$date == unique(stock$date)[i] & stock$sign < 0),][1:l-1,]$hvol), 
+                           stock[which(stock$time == unique(stock$time)[j] & stock$date == unique(stock$date)[i] & stock$sign < 0),][l,]$quote *
+                             (k - sum(stock[which(stock$time == unique(stock$time)[j] & stock$date == unique(stock$date)[i] & stock$sign < 0),][1:l-1,]$dvol,
+                                      stock[which(stock$time == unique(stock$time)[j] & stock$date == unique(stock$date)[i] & stock$sign < 0),][1:l-1,]$hvol)))/k
+          break
+        }else{
+          #stock[which(stock$time == unique(stock$time)[j] & stock$date == unique(stock$date)[i]),][,3] = NA
         }
       }
       #Cálculo de la horquilla relativa
-      hor_rel[(j*i), m] = (a[(j*i), m] - b[(j*i), m])/q[j*i]
-      
+      hor_rel[n, m] = (a[n, m] - b[n, m])/q[n]
+      print(c(a[n,m],b[n,m],q[n],hor_rel[n,m]))
       m = m + 1
     }
+    n = n + 1
+    cont = cont + 1
+    print(cont)
   }
+  
 }
+stock = drop_na(stock)
+cont = 0
 
 
